0226d1fc17abc52e20ef46d90a603bdf20dc98e9
sucks555 <102584033+sucks-555@users.noreply.github.com>
2023-11-15 03:20:55 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function downloadFile(url, fileName, fileExtension) {
   fetch(url)
     .then(response => {
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error('ネットワークの応答が正常ではありませんでした');
       }
       return response.blob();
     })
@@ -17,7 +17,7 @@ function downloadFile(url, fileName, fileExtension) {
       document.body.removeChild(link);
     })
     .catch(error => {
-      console.error('error:',error);
+      console.error('エラー:', error);
     });
 }
 function conversion(files) {
@@ -26,7 +26,21 @@ function conversion(files) {
     downloadFile(file.url, `file${i}`, file.extension);
   }
 }
-
-// ファイルダウンロード例
+function getExtension(url) {
+  const pathArray = url.split('/');
+  const lastPath = pathArray[pathArray.length - 1];
+  const parts = lastPath.split('.');
+  if (parts.length > 1) {
+    const extension = parts[parts.length - 1];
+    return { url: url, extension: extension };
+  } else {
+    console.error('エラー: URLにファイル拡張子がありません');
+    return null;
+  }
+}
 const url = location.href;
-conversion([{ url: url, extension: 'jpg' }]);
+const fileData = getExtension(url);
+
+if (fileData) {
+  conversion([fileData]);
+}
